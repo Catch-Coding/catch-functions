30da9492ea97d8f9e393697d936c7daa4ca21818
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-26 10:45:00 -0400

    print

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { alert, ask } from "./io.js";
+import { alert, ask, print } from "./io.js";
 import { randomInt, random } from "./random.js"
 
 export default Object.freeze({
   alert,
   ask,
+  print,
   randomInt,
   random
 });
